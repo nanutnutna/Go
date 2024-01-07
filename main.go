ac6fd8de78b5fa6d3fd14bb1b7ca4fc76658fb7b
nanutnutna <jtnattapot@gmail.com>
2024-01-07 22:46:44 +0700

    mod main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
@@ -23,36 +22,79 @@ func Devisable(start, end int) []int {
 	return result
 }
 
+func containsDuplicate(nums []int) bool {
+	check := map[int]int{}
+	for _, val := range nums {
+		check[val] += 1
+	}
+	for _, v := range check {
+		if v > 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func solution(nums []int) int {
+	count := map[int]int{}
+	for _, val := range nums {
+		count[val] += 1
+	}
+
+	result := 0
+	for key, val := range count {
+		if key == val && val > result {
+			result = val
+		}
+	}
+
+	if result == 0 {
+		return 0
+	}
+	return result
+}
+
 func main() {
 
-	fmt.Printf("%#v\n", Devisable(2000, 3000))
+	/*
 
-	body := `[
-	{
-		"imdbID": "tt4154796",
-		"title": "Avengers: Endgame",
-		"year": 2019,
-		"rating": 8.4,
-		"genres": ["Action","Drama"],
-		"isSuperHero": true
-	},
-	{
-		"imdbID": "tt4154756",
-		"title" : "Avengers: Infinity War",
-		"year" : 2018,
-		"rating": 8.4,
-		"genres": ["Action", "Sci-Fi"],
-		"isSuperHero": true
-	}
-	]`
+		fmt.Printf("%#v\n", Devisable(2000, 3000))
 
-	ms := []movie{}
-	err := json.Unmarshal([]byte(body), &ms)
-	fmt.Printf("%#v\n", err)
-	fmt.Printf("%#v\n", ms)
+		body := `[
+		{
+			"imdbID": "tt4154796",
+			"title": "Avengers: Endgame",
+			"year": 2019,
+			"rating": 8.4,
+			"genres": ["Action","Drama"],
+			"isSuperHero": true
+		},
+		{
+			"imdbID": "tt4154756",
+			"title" : "Avengers: Infinity War",
+			"year" : 2018,
+			"rating": 8.4,
+			"genres": ["Action", "Sci-Fi"],
+			"isSuperHero": true
+		}
+		]`
 
-	for _, v := range ms {
-		fmt.Println(v.Title)
-	}
+		ms := []movie{}
+		err := json.Unmarshal([]byte(body), &ms)
+		fmt.Printf("%#v\n", err)
+		fmt.Printf("%#v\n", ms)
+
+		for _, v := range ms {
+			fmt.Println(v.Title)
+		}
+
+
+		nums := []int{1, 2, 3, 1}
+		fmt.Println(containsDuplicate(nums))
+	*/
+
+	//nums := []int{3, 8, 2, 3, 3, 2}
+	nums := []int{2, 2, 4, 4, 4}
+	fmt.Println(solution(nums))
 
 }
